1e3b9baba9c492f1238f450e98cc05ebe9c985fc
davidjayjackson <davidjayjackson@gmail.como>
2020-10-09 21:11:58 -0400

    Began analysis of best Mask data table.

--- IHME-Db-R.R
+++ IHME-Db-R.R
@@ -11,21 +11,22 @@ library(scales)
 db <- dbConnect(SQLite(), dbname="../COVIDDB/IHME.sqlite3")
 ##
 BMH<- read.csv("./DATA/Best_mask_hospitalization_all_locs.csv")
-summary(BMH$date)
 dbWriteTable(db, "BMH",BMH ,overwrite=TRUE)
 BMH$date <- as.Date(BMH$date)
+summary(BMH$date)
 ##
 RFHOSP <- read.csv("./DATA/Reference_hospitalization_all_locs.csv")
-summary(RFHOSP$date)
 dbWriteTable(db, "RFHOsP",RFHOSP ,overwrite=TRUE)
 RFHOSP$date <- as.Date(RFHOSP$date)
+summary(RFHOSP$date)
 ##
 STATS <- read.csv("./DATA/Summary_stats_all_locs.csv")
 # names(STATS)
 dbWriteTable(db, "STATS",STATS ,overwrite=TRUE)
 ##
 WORSE <- read.csv("./DATA/Worse_hospitalization_all_locs.csv")
-WORSE$date <- as.Date(WORSE$date)
+
 dbWriteTable(db, "WORSE",WORSE ,overwrite=TRUE)
+WORSE$date <- as.Date(WORSE$date)
 ##
 dbListTables(db)
